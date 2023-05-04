83c51ad89923d4d3212f3d1d850eb93e229c75b5
suchi <suchika.parekh@webiots.in>
2023-05-04 15:07:28 +0530

    home section

---
+++ assets/js/home-animation.js
@@ -0,0 +1,23 @@
+/**=====================
+     Laptop Animation
+==========================**/
+
+
+// laptop rotate effect
+const element = document.querySelector(".laptop-img");
+let rotationDegree = 60;
+
+window.addEventListener("scroll", () => {
+    rotationDegree = Math.max(0, 60 - (window.scrollY / window.innerHeight) * 160);
+    element.style.transform = `rotateX(${rotationDegree}deg)`;
+});
+
+
+// effect object rotate
+window.addEventListener("scroll", () => {
+    const rotationAngle = window.pageYOffset * 0.15;
+    const rotateObjects = document.querySelectorAll(".rotate-effect");
+    rotateObjects.forEach((object) => {
+        object.style.transform = `rotate(${rotationAngle}deg)`;
+    });
+});
