06683d33c49ba6b9ba44a6f5e28650f0ae080447
suchi <suchika.parekh@webiots.in>
2023-06-02 17:36:57 +0530

    rtl

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,9 +48,90 @@ window.addEventListener('load', () => {
 });
 
 
+/*===================== 
+02. Tap To Top Js 
+==========================*/
+const tapTopBtn = document.querySelector(".tap-to-top-button")
+const tapTopTopBox = document.querySelector(".tap-to-top-box")
+tapTopBtn?.addEventListener("click", function () {
+    window.scroll({
+        top: 0,
+        behavior: 'smooth'
+    });
+})
+if (tapTopTopBox) {
+    window.onscroll = function () {
+        if (pageYOffset >= 1000) {
+            tapTopTopBox.classList.remove("hide")
+        } else {
+            tapTopTopBox.classList.add("hide")
+        }
+    }
+}
 
 
 
+
+/*===================
+18. Theme Setting js
+=======================*/
+const body = document.querySelector("body");
+const themeSettingMarkup = `
+<!-- Theme Setting Star -->
+<div class="theme-controller">
+    <div class="light-dark-box">
+            <button id="rtl-btn" class="btnTheme "><i data-feather="repeat"></i> <span class="text-value">Rtl</span></button>
+    </div>
+</div>
+<!-- Theme Setting End -->  
+`;
+
+body.insertAdjacentHTML("afterbegin", themeSettingMarkup);
+const themeBtnParent = document.querySelector(".light-dark-box");
+
+const rtlBtn = document.querySelector("#rtl-btn");
+const html = document.querySelector("html");
+const rtlLink = document.querySelector("#rtl-link");
+
+
+const activeRemoveFn = function (elList) {
+    elList.forEach(el => {
+        el.classList.remove("active");
+    })
+}
+
+themeBtnParent?.addEventListener("click", function (e) {
+    e.preventDefault();
+    const clicked = e.target.closest(".btnTheme")?.id;
+    if (!clicked) return;
+    if (clicked === "rtl-btn") {
+        rtlBtn.id = "ltr-btn";
+        rtlBtn.querySelector(".text-value").textContent = "Ltr"
+        html.setAttribute("dir", "rtl")
+        rtlLink.href = "assets/css/vendors/bootstrap.rtl.css";
+        rtlBtn.classList.add("rtlBtnEl")
+        localStorage.setItem('rtlcss', 'assets/css/vendors/bootstrap.rtl.css');
+        localStorage.setItem('dir', 'rtl');
+        localStorage.setItem('rtlBtnId', 'ltr-btn');
+        localStorage.setItem('textContentRtl', 'Ltr');
+    }
+    if (clicked === "ltr-btn") {
+        rtlBtn.id = "rtl-btn"
+        rtlBtn.querySelector(".text-value").textContent = "Rtl"
+        html.setAttribute("dir", "")
+        rtlLink.href = "assets/css/vendors/bootstrap.css";
+        localStorage.setItem('rtlcss', 'assets/css/vendors/bootstrap.css');
+        localStorage.setItem('dir', '');
+        localStorage.setItem('rtlBtnId', 'rtl-btn');
+        localStorage.setItem('textContentRtl', ' Rtl');
+    }
+})
+
+// Rtl 
+rtlBtn.id = localStorage.getItem("rtlBtnId") ? localStorage.getItem("rtlBtnId") : "rtl-btn";
+rtlBtn.querySelector(".text-value").textContent = localStorage.getItem("textContentRtl") ? localStorage.getItem("textContentRtl") : "Rtl";
+html.setAttribute("dir", localStorage.getItem("dir"));
+rtlLink.href = localStorage.getItem('rtlcss') ? localStorage.getItem('rtlcss') : 'assets/css/vendors/bootstrap.css';
 
 
 
