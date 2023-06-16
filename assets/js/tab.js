add045007f3f5a232b3bbd6f1e2a450407533649
suchi <suchika.parekh@webiots.in>
2023-06-16 09:28:10 +0530

    doc

---
+++ assets/js/tab.js
@@ -0,0 +1,10 @@
+
+
+function loginClick() {
+    var loginTab = document.getElementById('login-tab');
+    loginTab.click();
+}
+function signupClick() {
+    var signupTab = document.getElementById('signup-tab');
+    signupTab.click();
+}
